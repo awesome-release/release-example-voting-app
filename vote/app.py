cbb5ec90a8fbf300db0ca02218a863f0a5145e8e
David Giffin <david@giffin.org>
2026-09-23 18:58:24 -0400

    test: Base v4 / Retention v4 (webhook retention e2e)

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Base v3")
-option_b = os.getenv('OPTION_B', "Retention v3")
+option_a = os.getenv('OPTION_A', "Base v4")
+option_b = os.getenv('OPTION_B', "Retention v4")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
